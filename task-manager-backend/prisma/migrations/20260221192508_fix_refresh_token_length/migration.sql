f6fd7a826b0ce29223d74cda466d886b703c1e1a
the-paranoid07 <anishpal9621@gmail.com>
2026-02-22 03:35:43 +0530

    Reset migrations for clean production deploy

--- task-manager-backend/prisma/migrations/20260221192508_fix_refresh_token_length/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `user` MODIFY `refreshToken` TEXT NULL;
--- task-manager-backend/prisma/migrations/20260221220524_init/migration.sql
+++ task-manager-backend/prisma/migrations/20260221220524_init/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `refreshToken` VARCHAR(191) NULL,
+    `refreshToken` TEXT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `User_email_key`(`email`),
